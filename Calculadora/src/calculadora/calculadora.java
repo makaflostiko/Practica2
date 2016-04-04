987e486d150a1877e5f59e0362cd795de16fe77b
davidpagola <xerot1986@gmail.com>
2016-04-04 13:23:07 +0200

    Creacion de clase calculadora

---
+++ Calculadora/src/calculadora/calculadora.java
@@ -0,0 +1,25 @@
+package calculadora;
+
+public class calculadora {
+
+	
+	
+	private int num1;
+	private int num2;
+	
+	
+	
+	public calculadora(){
+		
+		num1=0;
+		num2=0;
+	}
+	
+	public calculadora(int num1, int num2){
+		
+		this.num1=num1;
+		this.num2=num2;
+		
+	}
+	
+}
